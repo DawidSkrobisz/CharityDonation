777d9f04741fcb6c242df6489310bdeb36c5aa34
DawSkr <dawidskrobisz88@gmail.com>
2023-09-19 15:45:35 +0200

    entityAndRepository

---
+++ src/main/java/pl/coderslab/charity/entity/Donation.java
@@ -0,0 +1,44 @@
+package pl.coderslab.charity.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Donation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    Integer quantity;
+
+    @ManyToMany
+    @JoinTable(name = "donation_category",
+            joinColumns = @JoinColumn(name = "donation_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private List<Category> categories;
+
+    @ManyToOne
+    @JoinColumn(name = "institution_id")
+    private Institution institution;
+    @NotNull
+    String street;
+    @NotNull
+    String city;
+    @NotNull
+    String zipCode;
+    @NotNull
+    LocalDate pickUpDate;
+    @NotNull
+    LocalTime pickUpTime;
+
+    String pickUpComment;
+
+}
